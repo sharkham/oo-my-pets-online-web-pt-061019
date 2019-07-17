8f6b5d406f95a176c71ec9276273d7f7e3aa7181
Sam Markham <sam.e.markham@gmail.com>
2019-07-17 00:38:26 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,18 @@
 class Cat
-  # code goes here
+  attr_reader :name
+  attr_accessor :mood, :owner 
+
+  @@all = []
+
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    self.class.all << self
+    @owner.cats << self unless @owner.cats.include?(self)
+  end
+
+  def self.all
+    @@all
+  end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,72 @@
+require 'pry'
+
 class Owner
-  # code goes here
+  attr_reader :name, :species, :cats, :dogs
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    self.class.all << self
+    @cats = []
+    @dogs = []
+  end
+
+  def say_species
+    "I am a #{species}."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    self.all.count
+  end
+
+  def self.reset_all
+    self.all.clear
+  end
+
+  def buy_cat(name)
+    Cat.new(name, self)
+    #putting the cat into owner's cats array happens in the Cat class
+  end
+
+  def buy_dog(name)
+    Dog.new(name, self)
+  end
+
+  def walk_dogs
+    self.dogs.each do |dog|
+      dog.mood = "happy"
+    end
+  end
+
+  def feed_cats
+    self.cats.each do |cat|
+      cat.mood = "happy"
+    end
+  end
+
+  def sell_pets
+    self.cats.each do |cat|
+      cat.mood = "nervous"
+      cat.owner = nil
+    end
+    self.dogs.each do |dog|
+      dog.mood = "nervous"
+      dog.owner = nil
+    end
+    self.cats.clear
+    self.dogs.clear
+  end
+
+  #challenge: try to come back to this and write a method that sells each pet by name 
+
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end
+
 end
